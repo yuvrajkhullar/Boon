dc0839cdabe07fef2e10845010b9ddd5737400e7
yrsingh1223 <44328803+yrsingh1223@users.noreply.github.com>
2019-11-01 19:37:53 -0700

    added checking password functionality

--- app/src/main/java/com/example/boon/MainActivity.java
+++ app/src/main/java/com/example/boon/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.content.Intent;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.boon.ui.main.ServicesFragment;
 
@@ -14,19 +16,33 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState); //will create an instance state of the onCreate method
-        setContentView(R.layout.activity_main); //will set the screen to the layout file that this java file corresponds to it
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
     }
 
     public void onClick(View view)
     {
-        Intent i = new Intent(this, ActionScreen.class); //creates an Intent for the button that will lead to the ActionScreen
-        startActivity(i); //will start the activity above
+        EditText mUser = (EditText)findViewById(R.id.email);
+        EditText mPass = (EditText)findViewById(R.id.password);
+        String userText = mUser.getText().toString();
+        String passText = mPass.getText().toString();
+
+        Intent i = new Intent(this, ActionScreen.class);
+
+        if (userText.equals("test") && passText.equals("test123"))
+        {
+            startActivity(i);
+        } else {
+            Toast toast = Toast.makeText(getApplicationContext(),
+                    "Incorrect Password",
+                    Toast.LENGTH_SHORT);
+            toast.show();
+        }
     }
 
     public void onClick2(View view)
     {
-        Intent j = new Intent(this, Register2.class); //creates an Intent for the button that will lead to the first Register screen
-        startActivity(j); //will start the activity above
+        Intent j = new Intent(this, Register2.class);
+        startActivity(j);
     }
 }
